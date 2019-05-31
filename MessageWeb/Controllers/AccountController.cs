9c7d1657d30020bed8d6e1d3a5479fae7d4ecd1d
miklos_kanyo <miklos_kanyo@hotmail.com>
2019-05-31 14:58:26 +0200

    Added FirstName, LastName to DbContext

--- MessageWeb/Controllers/AccountController.cs
+++ MessageWeb/Controllers/AccountController.cs
@@ -151,7 +151,15 @@ namespace Possible.MessageWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, PhoneNumber = model.PhoneNumber };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ MessageWeb/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Possible.MessageWeb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Possible.MessageWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Possible.MessageWeb.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
